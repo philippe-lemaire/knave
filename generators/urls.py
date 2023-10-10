0f57fa91761b9770ef041795378cd80bd59860ce
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-10-10 12:17:57 +0200

    new travel_hasard and weather views

--- generators/urls.py
+++ generators/urls.py
@@ -6,4 +6,6 @@ app_name = "generators"
 
 urlpatterns = [
     path("player-character", views.create_player_character, name="player-character"),
+    path("weather-forecast", views.roll_weather_view, name="weather-forecast"),
+    path("travel-hasard", views.roll_travel_hazard_view, name="travel-hazards"),
 ]
--- generators/views.py
+++ generators/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from game_logic.roll_weather import roll_weather
+from game_logic.roll_travel_hazards import roll_travel_hazards
 
 # Create your views here.
 
 
 def create_player_character(request):
     return None
+
+
+def roll_weather_view(request):
+    weather = roll_weather()
+    context = {"weather": weather}
+    template_name = "generators/weather.html"
+    return render(request, template_name, context)
+
+
+def roll_travel_hazard_view(request):
+    travel_hazard = roll_travel_hazards()
+    context = {"travel_hazard": travel_hazard}
+    template_name = "generators/travel_hazard.html"
+    return render(request, template_name, context)
