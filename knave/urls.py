5ecc2113a73cca5382d9d9045ce89338971dd284
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-11-18 18:20:22 +0100

    static in url

--- knave/urls.py
+++ knave/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("admin/", admin.site.urls),
     path("monsters/", include("monsters.urls")),
     path("generators/", include("generators.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
